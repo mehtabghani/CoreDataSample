2039580662c121931b75f0e00a19abb38111c616
mehtab <mehtab.ghani@gmail.com>
2015-11-09 15:32:52 +0500

    project setup

---
+++ CoreDataSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataSample
+//
+//  Created by mehtab on 11/9/15.
+//  Copyright (c) 2015 mehtab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreDataSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreDataSample
+//
+//  Created by mehtab on 11/9/15.
+//  Copyright (c) 2015 mehtab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
